859a176bf7928b0b8684014c70644e3085d4eb9e
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2022-12-17 12:05:57 -0500

    update application.rb file

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000', 'https://boiling-mountain-17336.herokuapp.com/' #replace this url with that of your own heroku client app
+        resource '*', :headers => :any, :methods => [:get]
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
